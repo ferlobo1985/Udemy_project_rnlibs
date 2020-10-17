4489653d952d4ae5f2a01aa178d9dccd5ac8597d
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-17 20:40:11 -0300

    Using contacts library

--- App.js
+++ App.js
@@ -2,35 +2,96 @@ import React, { useState, useEffect  } from 'react';
 import {
   Text,View,
   Image,StyleSheet, 
-  Button, PermissionsAndroid
+  Button, PermissionsAndroid,
+  Platform
 } from 'react-native';
-import Contacts from 'react-native-contacts'
+import Contacts, { addContact } from 'react-native-contacts'
 
 import Picker from './src/picker'
 
 const App = () => {
+  const [contacts,setContacts] = useState([])
 
-  useEffect(()=>{
-   PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
-      {
-        'title': 'Contacts',
-        'message': 'This app would like to view your contacts.',
-        'buttonPositive': 'Please accept bare mortal'
+
+  const requestPermission = async() => {
+    try{
+      if(Platform.OS === 'ios'){
+        return true;
+      } else {
+        const granted = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+          PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS
+          ],
+          {
+            'title': 'Contacts',
+            'message': 'This app would like to view your contacts.',
+            'buttonPositive': 'Please accept bare mortal'
+          }
+        );
+        return granted['android.permission.WRITE_CONTACTS'] === PermissionsAndroid.RESULTS.GRANTED
       }
-    )
-    .then(()=>{
+    } catch(err){
+      console.warn(err)
+    }
+  }
 
-      Contacts.getAll().then( contacts =>{
-        console.log(contacts)
-      }).catch(err=> console.log(err))
-      
+  const getContacts = () => {
+    requestPermission().then( didGetPermission => {
+      if(didGetPermission){
+        Contacts.getAll().then( contacts => {
+          setContacts(contacts)
+          //console.log(contacts);
+        })
+      }
     })
-  },[])
+  }
+
+  const addContact = () => {
+    requestPermission().then( didGetPermission => {
+      if(didGetPermission){
+        const newContact = {
+          familyName:"Steve",
+          givenName:"Jones"
+        };
+        Contacts.addContact(newContact).then(()=>{
+          getContacts();
+        })
+      }
+    })
+  }
 
   return(
-    <View>
-      <Picker/>
+    <View style={{marginTop:50}}>
+      
+      <Button
+        title="get contacts"
+        onPress={()=>getContacts()}
+      />
+      <Button
+        title="get contacts"
+        onPress={()=>addContact()}
+      />
+      { 
+       contacts.map((item)=>(
+         <View
+            key={item.recordID}
+            style={{
+              padding:10,
+              borderBottomColor:'red',
+              borderBottomWidth:1
+            }}
+         >
+          <Text>Name:{item.familyName}</Text>
+          <Text>Name:{item.givenName}</Text>
+          <Text>Email:{
+            item.emailAddresses[0] ?
+              item.emailAddresses[0].email
+              :
+              null
+          }</Text>
+         </View>
+       ))
+      }
     </View>
   )
 }
