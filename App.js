4de65a51baae9d4c2270b29524871dafd097a0db
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-17 05:18:12 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+const App = () => {
+  return(
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )
+}
+
+export default App;
